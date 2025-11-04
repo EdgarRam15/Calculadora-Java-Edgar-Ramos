349e7e7a970fc4673b8f57e4d51ad614e1fac9de
eramirez <eramirez@whatsbot.com.mx>
2025-11-04 16:38:54 -0600

    test y ci

--- src/Calculadora.java
+++ src/Calculadora.java
@@ -1,9 +1,19 @@
-public class Calculadora { 
- public static void main(String[] args) { 
- int resultado = sumar(5, 3); 
- System.out.println("Resultado: " + resultado); 
- } 
- public static int sumar(int a, int b) { 
- return a + b; 
- } 
+public class Calculadora {
+    public static int sumar(int a, int b) {
+        return a + b;
+    }
+
+    public static int restar(int a, int b) {
+        return a - b;
+    }
+
+    public static int multiplicar(int a, int b) {
+        return a * b;
+    }
+
+    public static int dividir(int a, int b) {
+        if (b == 0) throw new ArithmeticException("No se puede dividir entre cero");
+        return a / b;
+    }
 }
+
